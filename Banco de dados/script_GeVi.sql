33b2efea0ad9af9d2e96f13b8897a9c6822ca9c3
geovanna-v <geovanna.oliveira@sptech.school>
2025-11-13 21:49:00 -0300

    Backups

---
+++ Banco de dados/script_GeVi.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE GeVi;
+USE GeVi;
+
+CREATE TABLE usuario(
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome_completo VARCHAR(50),
+email VARCHAR(45),
+senha VARCHAR(10)
+);
+
+CREATE TABLE quiz(
+idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+pergunta VARCHAR(100)
+);
+
+CREATE TABLE resultado(
+idResultado INT,
+id_usuario INT,
+id_quiz INT,
+pontuacao VARCHAR(5),
+duracao DECIMAL(2),
+posicao VARCHAR(2),
+CONSTRAINT fk_usuario
+	FOREIGN KEY (id_usuario) REFERENCES usuario(idUsuario),
+CONSTRAINT fk_quiz
+	FOREIGN KEY (id_quiz) REFERENCES quiz(idQuiz));
